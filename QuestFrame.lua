a24073e5530ad86b1dee27b4f0508deb173fc387
Bjarke Istrup Pedersen <gurli@gurlinet.dk>
2020-10-16 15:39:34 +0200

    Changes needed to fix shift clicking quests in the quest list

--- QuestFrame.lua
+++ QuestFrame.lua
@@ -169,22 +169,23 @@ local function TitleButton_OnClick(self, button)
 	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
 	if ( not ChatEdit_TryInsertQuestLinkForQuestID(self.questID) ) then
 		PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+		local watchType = C_QuestLog.GetQuestWatchType(self.questID);
 		
 		if ( button == "RightButton" ) then
 			if ( self.mapID ) then
 				QuestMapFrame:GetParent():SetMapID(self.mapID)
 			end
 		elseif IsShiftKeyDown() then
-			if IsWorldQuestHardWatched(self.questID) or (IsWorldQuestWatched(self.questID) and GetSuperTrackedQuestID() == self.questID) then
+			if watchType == Enum.QuestWatchType.Manual or (watchType == Enum.QuestWatchType.Automatic and C_SuperTrack.GetSuperTrackedQuestID() == self.questID) then
 				BonusObjectiveTracker_UntrackWorldQuest(self.questID);
 			else
-				BonusObjectiveTracker_TrackWorldQuest(self.questID, true)
+				BonusObjectiveTracker_TrackWorldQuest(self.questID, Enum.QuestWatchType.Manual)
 			end
 		else
-			if IsWorldQuestHardWatched(self.questID) then
-				SetSuperTrackedQuestID(self.questID);
+			if watchType == Enum.QuestWatchType.Manual then
+				C_SuperTrack.SetSuperTrackedQuestID(self.questID);
 			else
-				BonusObjectiveTracker_TrackWorldQuest(self.questID)
+				BonusObjectiveTracker_TrackWorldQuest(self.questID, Enum.QuestWatchType.Automatic)
 			end
 		end
 	end
